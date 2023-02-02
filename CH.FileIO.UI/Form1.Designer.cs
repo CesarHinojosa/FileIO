e663ccb9667420a46a5af5bf814e2cb7d0c3d812
Cesar Hinojosa <300077836@fvtc.edu>
2023-02-02 13:16:19 -0600

    Add project files.

---
+++ CH.FileIO.UI/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace CH.FileIO.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuWordWrap = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.mnuPath = new System.Windows.Forms.ToolStripStatusLabel();
+            this.staSpring = new System.Windows.Forms.ToolStripStatusLabel();
+            this.staDateTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtDocument = new System.Windows.Forms.TextBox();
+            this.staFile = new System.Windows.Forms.ToolStripStatusLabel();
+            this.mnuMain.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuEdit});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(800, 28);
+            this.mnuMain.TabIndex = 0;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNew,
+            this.mnuOpen,
+            this.mnuSave,
+            this.mnuSaveAs,
+            this.toolStripMenuItem2,
+            this.mnuExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(46, 24);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuNew
+            // 
+            this.mnuNew.Name = "mnuNew";
+            this.mnuNew.Size = new System.Drawing.Size(148, 26);
+            this.mnuNew.Text = "&New";
+            this.mnuNew.Click += new System.EventHandler(this.mnuNew_Click);
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(148, 26);
+            this.mnuOpen.Text = "&Open";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(148, 26);
+            this.mnuSave.Text = "&Save";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // mnuSaveAs
+            // 
+            this.mnuSaveAs.Name = "mnuSaveAs";
+            this.mnuSaveAs.Size = new System.Drawing.Size(148, 26);
+            this.mnuSaveAs.Text = "&SaveAs...";
+            this.mnuSaveAs.Click += new System.EventHandler(this.mnuSaveAs_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(145, 6);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(148, 26);
+            this.mnuExit.Text = "&Exit";
+            // 
+            // mnuEdit
+            // 
+            this.mnuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuWordWrap});
+            this.mnuEdit.Name = "mnuEdit";
+            this.mnuEdit.Size = new System.Drawing.Size(49, 24);
+            this.mnuEdit.Text = "&Edit";
+            // 
+            // mnuWordWrap
+            // 
+            this.mnuWordWrap.Checked = true;
+            this.mnuWordWrap.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.mnuWordWrap.Name = "mnuWordWrap";
+            this.mnuWordWrap.Size = new System.Drawing.Size(168, 26);
+            this.mnuWordWrap.Text = "&Word Wrap";
+            this.mnuWordWrap.Click += new System.EventHandler(this.wordWrapToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuPath,
+            this.staFile,
+            this.staSpring,
+            this.staDateTime});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // mnuPath
+            // 
+            this.mnuPath.Name = "mnuPath";
+            this.mnuPath.Size = new System.Drawing.Size(0, 18);
+            // 
+            // staSpring
+            // 
+            this.staSpring.Name = "staSpring";
+            this.staSpring.Size = new System.Drawing.Size(746, 18);
+            this.staSpring.Spring = true;
+            // 
+            // staDateTime
+            // 
+            this.staDateTime.Name = "staDateTime";
+            this.staDateTime.Size = new System.Drawing.Size(0, 18);
+            // 
+            // txtDocument
+            // 
+            this.txtDocument.Location = new System.Drawing.Point(0, 31);
+            this.txtDocument.Multiline = true;
+            this.txtDocument.Name = "txtDocument";
+            this.txtDocument.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtDocument.Size = new System.Drawing.Size(800, 346);
+            this.txtDocument.TabIndex = 2;
+            this.txtDocument.TextChanged += new System.EventHandler(this.txtDocument_TextChanged);
+            // 
+            // staFile
+            // 
+            this.staFile.Name = "staFile";
+            this.staFile.Size = new System.Drawing.Size(0, 16);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDocument);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.mnuMain);
+            this.MainMenuStrip = this.mnuMain;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            //this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip mnuMain;
+        private ToolStripMenuItem mnuFile;
+        private ToolStripMenuItem mnuNew;
+        private ToolStripMenuItem mnuOpen;
+        private ToolStripMenuItem mnuSave;
+        private ToolStripMenuItem mnuSaveAs;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel staDateTime;
+        private ToolStripStatusLabel staSpring;
+        private ToolStripStatusLabel mnuPath;
+        private TextBox txtDocument;
+        private ToolStripMenuItem mnuEdit;
+        private ToolStripMenuItem mnuWordWrap;
+        private ToolStripSeparator toolStripMenuItem2;
+        private ToolStripMenuItem mnuExit;
+        private ToolStripStatusLabel staFile;
+    }
+}
---
+++ CH.FileIO.UI/Form1.cs
@@ -0,0 +1,157 @@
+using System.DirectoryServices;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+namespace CH.FileIO.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Global Variables
+        bool FirstSave = true;
+        //file  name uknown due to no file being created or opened
+        string FILE_NAME = "Untitled";
+        //starting file path unknown due to no file being created or opened
+        string FILE_PATH = "";
+        
+
+        //copied from code worked together
+        private void wordWrapToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtDocument.WordWrap = mnuWordWrap.Checked;
+        }
+        //private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        //{
+        
+        //}
+
+        //when the form is loaded the following happens
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = FILE_NAME;
+            staDateTime.Text = DateTime.Now.ToString();
+        }
+
+        
+        private void mnuNew_Click(object sender, EventArgs e)
+        {
+            //if the first save IS NOT TRUE
+            if (!FirstSave)
+            {
+                //Ask the user if they would like to save
+                DialogResult dlgResult = MessageBox.Show("Would you like to save?", "Unsaved Changed", MessageBoxButtons.YesNo);
+                if (dlgResult == DialogResult.Yes)
+                {
+                    mnuSaveAs_Click(sender, e);
+                    FirstSave = true;
+                }
+                //if user says no it clears everything
+                if (dlgResult == DialogResult.No)
+                {
+                    txtDocument.Clear();
+                    this.Text = "Untitled";
+                    staFile.Text = "";
+       
+                }
+            } 
+            //if the file is saved and the user exits it clears everything 
+            else if (FirstSave)
+            {
+                txtDocument.Clear();
+                this.Text = "Untitled";
+                staFile.Text = "";
+            }
+
+        }
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+
+            //this creates a new File Dialog
+            OpenFileDialog dlgOpen = new OpenFileDialog();
+
+            //The filter on the bottom right
+            dlgOpen.Filter = "Text Files|*.txt|AllFiles|*.*";
+
+            //This shows the Open Result File
+            DialogResult dlgResult = dlgOpen.ShowDialog();
+
+            if (dlgResult == DialogResult.OK)
+            {
+
+                using (StreamReader reader = new StreamReader(dlgOpen.FileName))
+                {
+                    txtDocument.Text = reader.ReadToEnd();
+                    //the file path is converted to the absolute directory 
+                    //THIS IS NEEDED FOR IT TO NOT CREATE A NEW FILE EVERYTIME YOU OPEN IT AND TRY TO SAVE IT
+                    FILE_PATH = dlgOpen.FileName;
+
+                    FILE_NAME = dlgOpen.SafeFileName;
+
+                    //The Form will be called to whatever the the user saved the FILE_NAME as
+                    this.Text = FILE_NAME;
+                    //this ties back to the File_path, the status strip that contains the file outputs a string of the File Path.
+                    staFile.Text = FILE_PATH;
+                    FirstSave = true;
+
+                }
+            }
+        }
+        private void mnuSave_Click(object sender, EventArgs e)
+        {   
+            //if the FILE_PATH 'IS NOT' a string 
+            if (FILE_PATH != "") 
+            {
+                //then the following text will be passed through and saved onto the FILE_PATH (which has already been created)
+                //The file path helps the computer know "hey this file exists ill just save the changes to the new file" instead of trying to create a new file 
+                using (StreamWriter writer = new StreamWriter(FILE_PATH))
+                {
+                    writer.Write(txtDocument.Text);
+                    //The file name of the text 
+                    //whatever the user put the file name in the (SAVE_AS method) it will be outputed onto the form
+                    this.Text = FILE_NAME;
+                    //the FILE_PATH that was taken from (SAVE_AS method) will be inserted onto the status strip file
+                    staFile.Text = FILE_PATH;
+                    //due to the user manually saving this and the file already created (the First Save is true)
+                    FirstSave = true;          
+                }  
+            }
+            //if the user has not saved the following text then it will push it to the Save As Method
+            else
+            {
+                mnuSaveAs_Click(sender, e);
+            }
+        }
+
+        //This saves the text to a file onto your computer
+        private void mnuSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlgSaveAs = new SaveFileDialog();
+            dlgSaveAs.Filter = "Text Files|*.txt|AllFiles|*.*";
+            DialogResult dlgResult = dlgSaveAs.ShowDialog();
+
+            if (dlgResult == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(dlgSaveAs.FileName))
+                {
+                    writer.Write(txtDocument.Text);
+
+                    FILE_PATH = dlgSaveAs.FileName;
+
+                    FILE_NAME = Path.GetFileName(dlgSaveAs.FileName);
+
+                    this.Text = FILE_NAME;
+                    staFile.Text = FILE_PATH;
+                    FirstSave = true;
+                }
+            }
+        }
+
+        private void txtDocument_TextChanged(object sender, EventArgs e)
+        {
+            FirstSave = false;
+        }
+    }
+}
